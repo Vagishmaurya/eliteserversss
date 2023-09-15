1b9e109e2d80df1c571df85182f9e2b90701adb0
Vagishmaurya <89206689+Vagishmaurya@users.noreply.github.com>
2023-09-16 01:31:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://elite-client-vagish.vercel.app/",
+		origin:"https://elite-client-vagish.vercel.app",
 		credentials:true,
 	})
 )
